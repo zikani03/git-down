7ecf67f92c925095bbfcb7b26cda1717bfea1fd5
kwalter <walterkaunda@live.co.uk>
2022-04-09 20:40:10 +0200

    Implement a nicer CLI using clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,77 @@
 // git-down
 // Author: zikani03
 //
+extern crate clap;
 extern crate fs_extra;
+extern crate regex;
+extern crate tempfile;
 
 use std::fs;
 use std::path::{Path, PathBuf};
 
+use clap::{Arg, Command};
+
+mod errors;
 mod git;
 mod git_url_parser;
-mod errors;
 
 // git-down main
 fn main() -> Result<(), errors::GitDownError> {
-    let url = std::env::args().nth(1).unwrap();
-    let target = std::env::args().nth(2).unwrap();
-    let targets = Vec::from([String::from(target)]);
+    let matches = Command::new("git-down")
+        .version("0.3.0")
+        .about("Download files from a git repo like a boss")
+        .arg(
+            Arg::new("directory")
+                .short('d')
+                .long("directory")
+                .takes_value(true)
+                .help("Download into this directory instead of the default one"),
+        )
+        .arg(Arg::new("url").required(true))
+        .arg(
+            Arg::new("files")
+                .multiple_occurrences(true)
+                .max_occurrences(10)
+                .required(true),
+        )
+        .get_matches();
 
+    let url = matches.value_of("url").unwrap();
+    let targets: Vec<String> = matches
+        .values_of("files")
+        .unwrap()
+        .map(|file| String::from(file))
+        .collect();
+
+    // Clone repository
     let git_url = git_url_parser::parse_url(&url)?;
     let git_dir = git::sparse_checkout(git_url.clone(), targets)?;
 
-    let dest_path = PathBuf::from(&git_url.name);
+    let dest_path = if matches.is_present("directory") {
+        PathBuf::from(matches.value_of("directory").unwrap())
+    } else {
+        PathBuf::from(&git_url.name)
+    };
 
-    if !dest_path.exists() {
-        fs::create_dir(git_url.name).expect("Cannot create destination directory");
-    }
-
-    for dir in git_dir.contents() {
-        move_directory(dir.as_path(), dest_path.as_path());
-    }
+    move_files(&git_dir.contents(), &dest_path);
 
     Ok(())
 }
 
-
-fn move_directory(source_path: &Path , dest_path: &Path) {
+fn move_files(source_paths: &Vec<PathBuf>, dest_path: &Path) {
     let options = fs_extra::dir::CopyOptions::new();
-    let source = source_path.to_str().unwrap().to_string();
+
+    if !dest_path.exists() {
+        fs::create_dir(dest_path).expect("Cannot create destination directory");
+    }
+
     let dest = dest_path.to_str().unwrap().to_string();
+    let mut sources: Vec<String> = Vec::new();
 
-    fs_extra::dir::move_dir(source, dest, &options)
-        .expect(&format!("Failed to copy files to directory. Find the files here: {}.", source_path.display()));
+    for path in source_paths {
+        sources.push(path.to_str().unwrap().to_string());
+    }
+
+    fs_extra::move_items(&sources, &dest, &options)
+        .expect(&format!("Failed to move files to {}", dest));
 }
-
